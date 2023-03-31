47d6354f5ce806c863f36e56128dcb94cd460edf
Ednario Andrade <joseednario637@gmail.com>
2023-03-30 22:11:18 -0300

    feat: add route create categories

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
 import express from "express";
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
 
 const port = 3000;
+
+app.use(express.json());
+
+app.use("/categories", categoriesRoutes);
 
 app.listen(port, () => {
   console.log(`Server is runner on port ${port}`);
