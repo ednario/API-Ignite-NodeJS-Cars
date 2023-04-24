f411f54aa3783ec7956bd1ca9dec5c75dd6eeafd
Ednario Andrade <joseednario637@gmail.com>
2023-04-24 19:45:38 -0300

    refactor: routes

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import express from "express";
 
-import { categoriesRoutes } from "./routes/categories.routes";
-import { specificationsRoutes } from "./routes/specifications.routes";
+import { router } from "./routes";
 
 const app = express();
 
@@ -9,8 +8,7 @@ const port = 3000;
 
 app.use(express.json());
 
-app.use("/categories", categoriesRoutes);
-app.use("/specifications", specificationsRoutes);
+app.use(router);
 
 app.listen(port, () => {
   console.log(`Server is runner on port ${port}`);
