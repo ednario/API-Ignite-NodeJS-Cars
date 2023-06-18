da79842b662c6a7fbb5f4455fcbdaf63e7c5691c
Ednario Andrade <joseednario637@gmail.com>
2023-06-18 18:07:03 -0300

    chore: ensure error handling AppError

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
-import express from "express";
+import "reflect-metadata";
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
 
 import swaggerUi from "swagger-ui-express";
 import swaggerFile from "./swagger.json";
@@ -9,6 +11,8 @@ import "./shared/container";
 
 import { router } from "./routes";
 
+import { AppError } from "./errors/AppError";
+
 const app = express();
 
 const port = 3000;
@@ -18,6 +22,21 @@ app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error - ${err.message}`,
+    });
+  }
+);
 
 app.listen(port, () => {
   console.log(`Server is runner on port ${port}`);
